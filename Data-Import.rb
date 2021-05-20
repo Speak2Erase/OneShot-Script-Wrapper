7146354bdaba8d2347cf190e32af57af7532211e
Speak2Erase <matthew@nowaffles.com>
2021-05-20 10:22:47 -0700

    Update Data-Import.rb

--- Data-Import.rb
+++ Data-Import.rb
@@ -8,21 +8,21 @@ require "pathname"
 require_relative "Classnames"
 require_relative "Script-Handler"
 
-#progress = ProgressBar.create(
-#  format: "%a /%e |%B| %p%% %c/%C %r files/sec %t",
-#  starting_at: 0,
-#  total: nil,
-#  output: $stderr,
-#  length: 150,
-#  title: "Imported",
-#  remainder_mark: "\e[0;30m█\e[0m",
-#  progress_mark: "█",
-#  unknown_progress_animation_steps: ["==>", ">==", "=>="],
-#)
+progress = ProgressBar.create(
+  format: "%a /%e |%B| %p%% %c/%C %r files/sec %t",
+  starting_at: 0,
+  total: nil,
+  output: $stderr,
+  length: 150,
+  title: "Imported",
+  remainder_mark: "\e[0;30m█\e[0m",
+  progress_mark: "█",
+  unknown_progress_animation_steps: ["==>", ">==", "=>="],
+)
 Dir.mkdir "Data_Test" unless Dir.exists? "Data_Test"
 paths = Pathname.glob(("Data_JSON/" + ("*" + ".json")))
 count = paths.size
-#progress.total = count
+progress.total = count
 paths.each_with_index do |path, i|
   content = Hash.new
 
@@ -111,5 +111,5 @@ paths.each_with_index do |path, i|
   rxdata = File.open("Data_Test/" + name.sub_ext(".rxdata").to_s, "wb")
   rxdata.puts Marshal.dump(content)
 
-  #progress.increment
+  progress.increment
 end
