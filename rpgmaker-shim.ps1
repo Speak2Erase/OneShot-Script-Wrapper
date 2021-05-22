b575cf860b7415492fcca42745fbb2b4cdf2d01c
Speak2Erase <matthew@nowaffles.com>
2021-05-21 20:32:35 -0700

    Update rpgmaker-shim.ps1

--- rpgmaker-shim.ps1
+++ rpgmaker-shim.ps1
@@ -2,21 +2,23 @@
 $rubyArgs = @('.\data-extractor.rb', 'import') 
 ruby $rubyArgs
 
-$fileWatcher = new-object System.IO.FileSystemWatcher
-$fileWatcher.Path = ".\Data"
+$fileWatcher = New-Object System.IO.FileSystemWatcher
+$fileWatcher.Path = ".\Data\"
 $fileWatcher.Filter = "*.rxdata";
 $fileWatcher.IncludeSubdirectories = $true
 $fileWatcher.EnableRaisingEvents = $true
 
 $action = {
+    Write-Host "Changes detected, exporting Data..."
     $rubyArgs = @('.\data-extractor.rb', 'export') 
-    ruby $rubyArgs | Write-Output
+    ruby $rubyArgs
+    Get-Event | Remove-Event
 }
 
-Register-ObjectEvent $filewatcher “Created” -Action $action | Out-Null
-Register-ObjectEvent $filewatcher “Changed” -Action $action | Out-Null
-Register-ObjectEvent $filewatcher “Deleted” -Action $action | Out-Null
-Register-ObjectEvent $filewatcher “Renamed” -Action $action | Out-Null
+Register-ObjectEvent $filewatcher “Created” -Action $action
+Register-ObjectEvent $filewatcher “Changed” -Action $action 
+Register-ObjectEvent $filewatcher “Deleted” -Action $action 
+Register-ObjectEvent $filewatcher “Renamed” -Action $action 
 
 $rpgmakerpath = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Classes\RPGXP.Project\shell\open\command\' -Name '(Default)'
 $rpgmakerpath = $rpgmakerpath -replace '"%1"', ''
@@ -24,11 +26,19 @@ $rpgmakerpath = $rpgmakerpath -replace '"', ''
 
 Write-Output "Opening RPG Maker"
 
-Start-Process $rpgmakerpath '.\game.rxproj' -NoNewWindow -Wait
+Start-Process $rpgmakerpath '.\game.rxproj' -NoNewWindow
+Start-Sleep(1)
+$rpgmakeropen = get-process "RPGXP" -ErrorAction SilentlyContinue
+while($Null -ne $rpgmakeropen) {
+    $rpgmakeropen = get-process "RPGXP" -ErrorAction SilentlyContinue
+}
 
 Write-Output "RPG Maker closed"
+
+Get-EventSubscriber | Unregister-Event
+$fileWatcher.EnableRaisingEvents = $false
+$fileWatcher.Dispose()
 
 $rubyArgs = @('.\data-extractor.rb', 'import') 
 ruby $rubyArgs
 
-Get-EventSubscriber | Unregister-Event
